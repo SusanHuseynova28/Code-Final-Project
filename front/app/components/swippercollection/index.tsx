a7ed06d53824c16201229e60e75adfbc3df683ab
unknown <susen.huseynova.11@mail.ru>
2024-10-20 00:51:59 +0400

    hot-fix:fullscreenpages/swipper

--- front/app/components/swippercollection/index.tsx
+++ front/app/components/swippercollection/index.tsx
@@ -10,11 +10,9 @@ export default function FullScreenSwiper() {
   const secondSlideRef = useRef(null);
 
   const handleSlideChange = (swiper) => {
-    // Reset animation on the first slide
     if (swiper.activeIndex === 0 && firstSlideRef.current) {
       resetAnimation(firstSlideRef);
     }
-    // Reset animation on the second slide
     if (swiper.activeIndex === 1 && secondSlideRef.current) {
       resetAnimation(secondSlideRef);
     }
@@ -22,7 +20,6 @@ export default function FullScreenSwiper() {
 
   const resetAnimation = (slideRef) => {
     slideRef.current.classList.remove("animate-toss-up");
-    
     void slideRef.current.offsetWidth;
     slideRef.current.classList.add("animate-toss-up");
   };
@@ -37,7 +34,7 @@ export default function FullScreenSwiper() {
         className="w-full h-full"
         onSlideChange={handleSlideChange}
       >
-   
+        {/* First Slide */}
         <SwiperSlide>
           <div className="relative w-full h-full">
             <img
@@ -45,26 +42,23 @@ export default function FullScreenSwiper() {
               alt="Slide 1"
               className="w-full h-full object-cover"
             />
-            <div className="absolute inset-0 flex items-center justify-start px-44">
-              <div
-                ref={firstSlideRef}
-                className="text-content animate-toss-up "
-              >
-                <h1 className="text-6xl font-semibold text-gray-800 mt-24 ">
+            <div className="absolute inset-0 flex items-center justify-start px-8 md:px-44">
+              <div ref={firstSlideRef} className="text-content animate-toss-up">
+                <h1 className="text-6xl font-semibold text-gray-800 mt-20">
                   Spring Collection
                 </h1>
-                <div className="mt-10 w-[500px]">
-                  <p className="text-base text-gray-500 pl-10">
+                <div className="mt-10 w-[300px] md:w-[700px]">
+                  <p className="text-base text-gray-500">
                     In a world of hyper-convenience and overconsumption we want
                     to
                   </p>
-                  <p className="text-texthovercolor pl-28 mt-2">
+                  <p className="text-texthovercolor pl-6 md:pl-24 mt-2">
                     strip away complexity from your everyday.
                   </p>
                 </div>
-                <div className="flex justify-center items-center">
-                  <button className="bg-[#cea384] text-white px-6 py-2 text-lg mt-20 ">
-                  <Link href="/drowerfilter">Shop Now</Link>
+                <div className="pl-8 md:pl-44 mt-16">
+                  <button className="bg-[#cea384] text-white px-6 py-2 text-lg">
+                    <Link href="/drowerfilter">Shop Now</Link>
                   </button>
                 </div>
               </div>
@@ -72,7 +66,7 @@ export default function FullScreenSwiper() {
           </div>
         </SwiperSlide>
 
-       
+        {/* Second Slide */}
         <SwiperSlide>
           <div className="relative w-full h-full">
             <img
@@ -80,22 +74,28 @@ export default function FullScreenSwiper() {
               alt="Slide 2"
               className="w-full h-full object-cover"
             />
-            <div className="absolute inset-0 flex items-center justify-end px-10">
+            <div className="absolute inset-0 flex items-center justify-end px-8 md:px-44">
               <div
                 ref={secondSlideRef}
-                className="text-right text-content animate-toss-up"
+                className="text-right text-content animate-toss-up mt-20"
               >
-                <h1 className="text-5xl font-bold text-gray-800 mb-4">
+                <h1 className="text-6xl font-semibold text-gray-800 mb-4 flex justify-start pl-16">
                   New Collection
                 </h1>
-                <p className="text-lg text-gray-500 mb-6">
-                  In a world of hyper-convenience and overconsumption,
-                  <br />
-                  we want to strip away complexity from your everyday.
-                </p>
-                <button className="bg-[#cea384] text-white px-6 py-2 text-lg font-semibold">
-                  <Link href="/drowerfilter">Shop Now</Link>
-                </button>
+                <div className="pl-10 flex flex-col gap-2">
+                  <p className="text-base text-gray-500 mt-4">
+                    In a world of hyper-convenience and overconsumption we want
+                    to,
+                  </p>
+                  <p className="text-center text-base text-gray-500">
+                    strip away complexity from your everyday.
+                  </p>
+                  <div className="flex justify-start pl-44 mt-16">
+                    <button className="bg-[#cea384] text-white px-6 py-2 text-lg">
+                      <Link href="/drowerfilter">Shop Now</Link>
+                    </button>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
